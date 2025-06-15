8fbc9ccf0b6c6e2e56536ced1e1812805caf89bc
arie-rodzi <zahari@uitm.edu.my>
2025-06-15 09:15:00 +0800

    Delete pages/1.isi_maklumat_peribadi.py

--- pages/1.isi_maklumat_peribadi.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-if st.session_state.get("user_role") != "pelajar":
-    st.warning("Modul ini hanya untuk pelajar.")
-    st.stop()
-
-st.title("Judul Modul")  # GANTI DENGAN TAJUK SEBENAR
