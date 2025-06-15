22ca0c9b78123b47a795ca8337e253ba3d4701a4
arie-rodzi <zahari@uitm.edu.my>
2025-06-15 10:56:05 +0800

    Update 4.cetak_surat_penempatan_sli03.py

--- pages/4.cetak_surat_penempatan_sli03.py
+++ pages/4.cetak_surat_penempatan_sli03.py
@@ -17,7 +17,7 @@ output_path = f"generated/surat_penempatan_{pelajar_id}.docx"
 
 os.makedirs("generated", exist_ok=True)
 
-conn = sqlite3.connect("database.db")
+conn = sqlite3.connect("database/latihan_industri.db")
 c = conn.cursor()
 
 # Pastikan maklumat tersedia
